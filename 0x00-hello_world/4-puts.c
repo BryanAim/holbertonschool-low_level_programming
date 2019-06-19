2d456b49efd54cd6951eddb5dca2c79228183339
JUCORTESA <984@holbertonschool.com>
2019-06-19 18:55:55 +0000

    delete space in comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-  * puts - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
+ * puts - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
