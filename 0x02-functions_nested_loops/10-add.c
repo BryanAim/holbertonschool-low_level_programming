8ca4921f29479a71642f4ebaf29b929d0bb8b8b5
JUCORTESA <984@holbertonschool.com>
2019-06-25 19:53:35 +0000

    Correction on result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,25 +1,17 @@
 #include "holberton.h"
 
 /**
-* add - adds two integers
+* add - sum two integers
 * @a: The first character to print
 * @b: The second character to print
-* @c, @x, @y: The character to print
-*
-* Return: 1 if is positive
-* If is 0 returns zero
-* Otherwise return -1.
+* Return: the results
 */
 
 int add(int a, int b)
 {
-	int c, x, y;
+	int c;
 
 	c = a + b;
-	x = c / 10;
-	y = c % 10;
 
-	_putchar(x + '0');
-
-	return (y);
+	return (c);
 }
