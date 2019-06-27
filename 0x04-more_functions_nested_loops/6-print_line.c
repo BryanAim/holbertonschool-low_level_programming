9a3006f0b607520b81ab3b3a9519ad6cb3524b9c
JUCORTESA <984@holbertonschool.com>
2019-06-27 20:09:48 +0000

    delete space before l

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,16 @@
 
 /**
   * print_line - draws a straight line
-  *
+  * @n: number of lines
   */
 
 void print_line(int n)
 {
 	int l;
 
-	for (l = 0 ; l <= n; l++)
+	for (l = 1; l <= n; l++)
 	{
-		_putchar('_');
+			_putchar('_');
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
