6c21c33c532beec4b41c90d31b1c405002a5cecb
JUCORTESA <984@holbertonschool.com>
2019-06-27 22:39:36 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		if (((n % 3) != 0) && ((n % 5) != 0))
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
