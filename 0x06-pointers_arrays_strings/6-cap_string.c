f1b1707b6dbee7e5e5270176f13f64fc01fc4c3d
JUCORTESA <984@holbertonschool.com>
2019-07-04 19:31:20 +0000

    capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,26 @@
 #include "holberton.h"
 
+/**
+  * *cap_string - capitalize words
+  * @s: array
+  * Return: uppercase
+  */
+
 char *cap_string(char *s)
 {
-	int i = 0; 
+	int i = 0;
 
-	while (s[i] !=  '\0')
+	while (s[i] != '\0')
 	{
-		while( s[i] == 32 || s[i] == 10 || s[i] == 9)
+		if (s[i - 1] == 32 || s[i - 1] == 10 || s[i - 1] == 9
+		|| s[i - 1] == 46)
 		{
-		if(s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-		s[i]= s[i] - 32;
+		s[i] = s[i] - 32;
 		}
+	}
 		i++;
-		}
 	}
 	return (s);
 }
