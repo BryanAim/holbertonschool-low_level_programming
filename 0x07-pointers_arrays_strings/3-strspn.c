d660ea5cd7e8c45b29446bb2812b635db07270b3
JUCORTESA <984@holbertonschool.com>
2019-07-08 23:32:00 +0000

    add break

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,21 +10,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, n = 0;
+	int i, j;
 
-	for (j = 0; accept[j] != '\0'; j++)
+	for (i = 0; s[i]; i++)
 	{
-	for (i = 0; s[i] != '\0'; i++)
+	for (j = 0; accept[j]; j++)
 	{
 		if (s[i] == accept[j])
 		{
-			if (i >= n)
-			{
-			n = i + 1;
-			}
+			break;
+		}
+	}
+		if (!accept[j])
+		{
 		break;
 		}
 	}
-	}
-	return (n);
+	return (i);
 }
