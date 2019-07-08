a03e66f5eb9c52cb49f0f11b8b67c75cb5090a86
JUCORTESA <984@holbertonschool.com>
2019-07-08 22:50:28 +0000

    add a pointer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,14 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
+	char *d = dest, *s = src;
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		*d = *s;
+		s++;
+		d++;
 	}
 	return (dest);
 }
