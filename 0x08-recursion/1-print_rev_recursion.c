0f9bd7e7663059d0096573f753c04e5e1cbb79db
JUCORTESA <984@holbertonschool.com>
2019-07-10 17:56:17 +0000

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  * _print_rev_recursion - reverse a string
+  * @s: parameter
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+	}
+		s--;
+		_putchar(*s);
+}
