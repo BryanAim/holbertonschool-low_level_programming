115e9b07e421e7ce224a3c7ed2f290856941cae9
JUCORTESA <984@holbertonschool.com>
2019-07-16 20:02:08 +0000

    2 dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,44 +2,33 @@
 #include <stdlib.h>
 
 /**
-  * *str_concat - concatenates two strings
-  * @s1: string
-  * @s2: string
+  * **alloc_grid - pointer 2 dimensional array
+  * @width: width
+  * @height: height
   * Return: pointer.
   */
 
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	char *s;
+	int **s;
+	int i;
 
-	int i = 0, sizes1 = 0;
-	int j = 0, sizes2 = 0;
-
-	while (*(s1 + i) != '\0')
-	{
-		i++;
-		sizes1++;
-	}
-	while (*(s2 + j) != '\0')
-	{
-		j++;
-		sizes2++;
-	}
-	s = (char *)malloc((sizes1 + sizes2) * sizeof(char));
+	if (width <= 0 || height <= 0)
+		return ('\0');
+	s = (int **)malloc(height * sizeof(int *));
 	if (s == NULL)
 	{
-		return (0);
+		return (NULL);
+		free(s);
 	}
-	else
+	for (i = 0; i < width; i++)
 	{
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		s[i] = s1[i];
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		s[i + j] = s2[j];
-	}
+		s[i] = (int *)malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			return (NULL);
+			free(s);
+		}
 	}
 	return (s);
 }
