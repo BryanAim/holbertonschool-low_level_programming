c24eb8688bd1438eccb4542df76a6bf7b8d9a789
JUCORTESA <984@holbertonschool.com>
2019-07-18 18:46:52 +0000

    just one for

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,16 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s = (char *)malloc((sizes1 + n + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-	if (n >= sizes2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			s[i] = s1[i];
-		for (j = 0; j <= (sizes2 + 1); j++)
-			s[i + j] = s2[j];
-		s[i + j] = '\0';
-	}
-	else
-	{
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
 	for (j = 0; j < n; j++)
@@ -49,6 +39,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i + j] = s2[j];
 	}
 	s[i + j] = '\0';
-	}
 	return (s);
 }
