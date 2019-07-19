b9ecdab6659fcb8d60d422ed5fb1c910a75d72df
JUCORTESA <984@holbertonschool.com>
2019-07-19 01:24:49 +0000

    change in malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (0);
 	}
 
-	ptr = malloc(new_size * sizeof(unsigned int));
+	ptr = malloc(new_size);
 
 	if (ptr == NULL)
 	{
