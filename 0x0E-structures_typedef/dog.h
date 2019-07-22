515a084aaf7c34a40bc5f8693e82a7020e57f6f8
JUCORTESA <984@holbertonschool.com>
2019-07-22 22:49:01 +0000

    create new structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -32,5 +32,6 @@ typedef struct dog_t
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
