3e5ec9aab6524753bdc54e450ec09ba60ad8be53
JUCORTESA <984@holbertonschool.com>
2019-07-24 18:38:10 +0000

    search for integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,11 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 		if (size < 0)
 			return (-1);
-		for (i = 0; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			cmp(array[i]);
-			if (cmp != 'NULL')
+			for (i = 0; i < size; i++)
+		{
+				cmp(array[i]);
+				if (cmp(array[i]) != 0)
 				return (i);
+		}
 		}
 		return (-1);
 }
