50d7ee21bec9169184123cb4860bcb904936d40f
JUCORTESA <984@holbertonschool.com>
2019-07-28 14:48:42 +0000

    add condition to print ,

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,40 +2,40 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
-  * print_strings - print strings
-  * @n: number of arguments
-  * @separator: string between numbeers
+  * print_all - print anything
+  * @format: type of argument
   */
 void print_all(const char * const format, ...)
 {
 	va_list arg;
-	int i = 0;
+	unsigned int i = 0;
 	char *str;
-	
+
 	va_start(arg, format);
-	while (format[i])
+	while (format[i] && format)
 	{
-	switch (format[i])
+	switch (format[i++])
 	{
 		case 'c':
-			printf("%c, ", va_arg(arg, int));
+			printf("%c", va_arg(arg, int));
 			break;
 		case 'i':
-			printf("%d, ", va_arg(arg, int));
+			printf("%d", va_arg(arg, int));
 			break;
 		case 'f':
-			printf("%f, ", va_arg(arg, double));
+			printf("%f", va_arg(arg, double));
 			break;
 		case 's':
 			str = va_arg(arg, char *);
 			if (arg == NULL)
-				printf("nil");
+				printf("(nil)");
 			printf("%s", str);
 			break;
 		default:
-				;
+			continue;
 	}
-	i++;
+	if (format[i])
+		printf(", ");
 	}
 	printf("\n");
 	va_end(arg);
