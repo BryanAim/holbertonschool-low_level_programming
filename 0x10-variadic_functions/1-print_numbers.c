3b772e158b2d889e5a4791d19bee6ed6a9f6691f
JUCORTESA <984@holbertonschool.com>
2019-07-28 13:23:57 +0000

    fix: null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,16 +12,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, var;
 
 	va_start(arg, n);
-	if (separator != NULL)
-	{
-		for (i = 0; i < n; i++)
-		{
-			var = va_arg(arg, unsigned int);
 
-			printf("%d", var);
-		if (i < n - 1 && separator)
+	for (i = 0; i < n; i++)
+	{
+		var = va_arg(arg, unsigned int);
+
+		printf("%d", var);
+		if ((i < n - 1) && separator)
 			printf("%s", separator);
-	}
 	}
 	printf("\n");
 	va_end(arg);
