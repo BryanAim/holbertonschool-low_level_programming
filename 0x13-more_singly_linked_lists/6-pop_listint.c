bf52d970c674909140ef9bb3e63b83af1bb644c8
JUCORTESA <984@holbertonschool.com>
2019-08-05 19:03:57 +0000

    [Add] free and delete head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head.
+ * @head: pointer to structure
+ * Return: the head node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int length = 0;
+
+	if ((*head)->next == NULL)
+	{
+		length = (*head)->n;
+		free(*head);
+		return (length);
+	}
+	else
+	{
+		temp = *head;
+		*head = (*head)->next;
+		length = temp->n;
+		free(temp);
+	}
+	return (length);
+}
