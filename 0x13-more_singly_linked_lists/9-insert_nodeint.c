0d866d4de8c29f5ea9229a669476ac257acbf5e1
JUCORTESA <984@holbertonschool.com>
2019-08-06 20:33:05 +0000

    [Fix] return value fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,11 +38,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			newNode->next = temp->next;
 			temp->next = newNode;
+			return (newNode);
 		}
 		temp = temp->next;
 		count++;
 	}
-	return (newNode);
-	}
 	return (NULL);
+	}
+
 }
