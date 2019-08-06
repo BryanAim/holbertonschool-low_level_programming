0648ebe0cc23ff9248daec8d392298d314d0f8e0
JUCORTESA <984@holbertonschool.com>
2019-08-06 19:12:09 +0000

    [Fix] a loop if index < counter

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,30 +9,38 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *temp2;
-	unsigned int count;
+	unsigned int count, count2 = 0;
 
-	if (head == NULL)
-		return (-1);
 	if (*head)
 	{
-	temp = *head;
-	temp2 = (*head)->next;
-
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-	}
-	else
-	{
-		for (count = 1; count < index; count++)
+		temp = *head;
+		while (temp != NULL)
 		{
 			temp = temp->next;
-			temp2 = temp->next;
+			count2++;
 		}
-		temp->next = temp2->next;
-		free(temp2);
+		if (index < count2)
+		{
+			temp = *head;
+			temp2 = (*head)->next;
+			if (index == 0)
+			{
+				*head = (*head)->next;
+				free(temp);
+			}
+			else
+			{
+				for (count = 0; count < (index - 1); count++)
+				{
+					temp = temp->next;
+					temp2 = temp->next;
+				}
+				temp->next = temp2->next;
+				free(temp2);
+			}
 		}
+		else
+			return (-1);
 	return (1);
 	}
 	else
