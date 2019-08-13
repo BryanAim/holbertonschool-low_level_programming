14f06368907b09180aa7034ea74c6360080887c3
JUCORTESA <984@holbertonschool.com>
2019-08-13 19:49:27 +0000

    [Fix] change exito to return

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,12 +18,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buff = malloc((letters + 1) * sizeof(char));
 	if (buff == NULL)
 		return (0);
-	
+
 	buff[letters] = '\0';
 	file = open(filename, O_RDONLY);
 	if (file == -1)
-		exit(0);
-	
+		return (0);
+
 	letters2 =  read(file, buff, letters);
 	write(STDOUT_FILENO, buff, letters2);
 	close(file);
