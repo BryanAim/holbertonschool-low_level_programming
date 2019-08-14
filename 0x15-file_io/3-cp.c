8b3e0f60cdbfc2adc55dfca07b4a62c9a9a6a5cc
JUCORTESA <984@holbertonschool.com>
2019-08-14 04:33:05 +0000

    [fix] change name in dprintf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,9 +15,10 @@ void not_close(int fd)
  * cp - copy a file
  * @from: file to copy
  * @to: file to copy in
+ * Return: 0.
  */
 
-void cp(const char *from, char *to)
+int cp(const char *from, char *to)
 {
 	int file_r, file_w, r;
 	char buff[1024];
@@ -25,32 +26,32 @@ void cp(const char *from, char *to)
 	file_r = open(from, O_RDONLY);
 	if (file_r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
 	file_w = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_w == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
 	r = read(file_r, buff, 1024);
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
 	while (r != 0)
 	{
 		if (write(file_w, buff, r) == -1)
 		{
-			dprintf(2, "Error: Can't write to %s", to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", to);
 			exit(99);
 		}
 		r = read(file_r, buff, 1024);
 		if (r == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", from);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
 			exit(98);
 		}
 	}
@@ -58,6 +59,7 @@ void cp(const char *from, char *to)
 	not_close(file_r);
 	if (close(file_r) == -1)
 	not_close(file_w);
+	return (0);
 }
 /**
  * main - copy a file into another
@@ -71,12 +73,12 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-	dprintf(2, "Usage: cp file_from file_to\n");
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 	exit(97);
 	}
 	if (argv[1] == NULL)
 	{
-	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 	}
 	cp(argv[1], argv[2]);
