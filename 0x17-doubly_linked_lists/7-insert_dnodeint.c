0ba923a5d9cd6f57447babefe8c171f8fc9584e8
JUCORTESA <984@holbertonschool.com>
2019-09-15 17:22:23 +0000

    Add edge cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,50 +2,47 @@
 /**
  * insert_dnodeint_at_index - insert node at index
  * @h: pointer to structure
- * @idx: index of the node
+ * @idx: index of the newnode
  * @n: data
- * Return: addres of the new node at index, NULL if failed
+ * Return: addres of the new newnode at index, NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *temp;
-	unsigned int count = 0;
+	unsigned int cont = 0;
+	dlistint_t *newnode, *temp, *aux;
 
-	if (h == NULL)
-		return (NULL);
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
-		return (NULL);
-	newnode->n = n;
-	newnode->next = NULL;
-	newnode->prev = NULL;
-	if (*h == NULL && idx != 0)
 	{
-		*h = newnode;
-		return (newnode);
+		free(newnode);
+		return (NULL);
 	}
 	if (idx == 0)
 	{
+		newnode->n = n;
 		newnode->next = *h;
+		newnode->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = newnode;
 		*h = newnode;
-		return (newnode);
+		return (*h);
 	}
-	else
-	{
 	temp = *h;
 	while (temp != NULL)
 	{
-		if (count == (idx - 1))
+		if (cont == idx - 1)
 		{
-			newnode->next = temp->next;
-			newnode->prev = temp->prev;
+			newnode->n = n;
+			aux = temp->next;
 			temp->next = newnode;
+			newnode->next = aux;
+			if (aux != NULL)
+				aux->prev = newnode;
+			newnode->prev = temp;
 			return (newnode);
 		}
+		cont++;
 		temp = temp->next;
-		count++;
 	}
 	return (NULL);
-	}
-
 }
