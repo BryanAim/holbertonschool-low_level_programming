7331e1a3af7c68ca5a55057b343777d343ce22e9
JUCORTESA <984@holbertonschool.com>
2019-09-15 03:20:14 +0000

    Fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,11 +6,11 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 1;
 
 	if (h == NULL)
 		return (0);
-	while (h != NULL)
+	while (h->next != NULL)
 	{
 		h = h->next;
 		i++;
