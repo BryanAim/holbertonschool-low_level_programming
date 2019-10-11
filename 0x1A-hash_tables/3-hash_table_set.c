a6bf09d1823368c99f4be789b42a1f7716719ce9
JUCORTESA <984@holbertonschool.com>
2019-10-11 22:14:15 +0000

    [fix] missin next in node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,44 +1,27 @@
 #include "hash_tables.h"
+
 /**
- * add_hash - adds a new node for the hash
+ * add_node - adds a new node for the hash
  * @head: pointer to the nodes
  * @key: the key, can not be an empty string
- * @value: value associated with the key
+ * @val: value associated with the key
  *
  * Return: new pointer to node
  */
-hash_node_t *add_hash(hash_node_t **head, const char *key, const char *value)
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *val)
 {
-	hash_node_t *newnode;
+	hash_node_t *new_node;
 
-	if (head == NULL)
-	{
-		newnode = malloc(sizeof(hash_node_t));
-		if (newnode == NULL)
-		{
-			free(newnode);
-			return (0);
-		}
-		newnode->key = strdup(key);
-		newnode->value = strdup(value);
-		*head = newnode;
-	}
-	else
-	{
-		newnode = malloc(sizeof(hash_node_t));
-		if (newnode == NULL)
-		{
-			free(newnode);
-			return (0);
-		}
-		newnode->key = strdup(key);
-		newnode->value = strdup(value);
-		newnode->next = (*head);
-		*head = newnode;
-	}
-
-	return (newnode);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(val);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
+
 /**
  * hash_table_set - a function that adds an element to the hash table
  * @ht: pointer to a hash table
@@ -52,13 +35,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL, *aux = NULL;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || value == NULL || key[0] == '\0')
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	aux = ht->array[index];
 	if (aux != NULL)
 	{
-		new = add_hash(&(ht->array[index]), key, value);
+		new = add_node(&(ht->array[index]), key, value);
 		if (new == NULL)
 		{
 			return (0);
@@ -66,22 +49,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		while (aux != NULL)
-		{
-			if (strcmp(aux->key, key) == 0)
-				break;
-			aux = aux->next;
-		}
+		new = add_node(&(ht->array[index]), key, value);
 		if (new == NULL)
 		{
-			new = add_hash(&(ht->array[index]), key, value);
-			if (new == 0)
-				return (0);
-		}
-		else
-		{
-			free(aux->value);
-			aux->value = strdup(value);
+			return (0);
 		}
 	}
 	return (1);
