63a7d51d8ee8f8dbc9f3abd86dd23c9c4e76198c
JUCORTESA <984@holbertonschool.com>
2019-10-11 00:10:44 +0000

    [fix] create hastable [Add] print and add element

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,23 +1,33 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: the size of the table
+ *
+ * Return: a pointer to a hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
+	hash_table_t *new_hash;
+	unsigned long int i = 0;
 
-	hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
-
-	if (hashtable == NULL)
-		return (NULL);
-	hashtable->array = malloc(sizeof(hash_node_t) * size);
-
-	if (hashtable->array == NULL)
+	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
 	{
-		free(hashtable->array);
 		return (NULL);
 	}
+	new_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new_hash->array[i] = NULL;
+		i++;
+	}
+	new_hash->size = size;
 
-	for (i = 0; i < size; ++i)
-		hashtable->array[i] = NULL;
-	return (hashtable);
+	return (new_hash);
 }
-
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,18 +1,43 @@
 #include "hash_tables.h"
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *add_hash_table(hash_node_t **head, const char *key, const char *value)
 {
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	hash_node_t *newnode;
+
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
 	{
 		free(newnode);
-		return (NULL);
+		return (0);
 	}
-	newnode->key = strup(key);
-	newnode->value = strup(value);
-	if (*ht != NULL)
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	if (*head != NULL)
 		newnode->next = NULL;
-	if (*ht == NULL)
+	if (*head == NULL)
 		newnode->next = NULL;
-	*ht = newnode;
-	return (*ht);
+	*head = newnode;
+	return (newnode);
+}
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new = NULL, *aux = NULL;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	aux = ht->array[index];
+	if (aux != NULL)
+	{
+		new = add_hash_table(&(ht->array[index]), key, value);
+		if (new == NULL)
+			return(0);
+	}
+	else
+	{
+		new = add_hash_table(&(ht->array[index]), key, value);
+		if (new == NULL)
+			return(0);
+	}
+	return (1);
 }
