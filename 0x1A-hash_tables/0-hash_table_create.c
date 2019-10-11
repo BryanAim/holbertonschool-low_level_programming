dee4aa54dff62334c5bf000509208c8abd39aafb
JUCORTESA <984@holbertonschool.com>
2019-10-11 17:11:02 +0000

    [Fix] add condition size < 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash;
 	unsigned long int i = 0;
 
+	if (size < 1)
+		return (NULL);
 	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 		return (NULL);
@@ -26,6 +28,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		i++;
 	}
 	new_hash->size = size;
-
 	return (new_hash);
 }
