1868a0bf6275745b6049679fb9c3c837ae6e6291
JUCORTESA <984@holbertonschool.com>
2019-10-27 21:14:12 +0000

    [Add] insertion sort

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Insert sort algorithm
+ * @list: Size of the array
+ * Return: Nothing.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp;
+
+
+	tmp = (*list)->next;
+	while (tmp != NULL)
+	{
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			if (tmp->next != NULL)
+			{
+				tmp->next->prev = tmp->prev;
+			}
+			tmp->prev->next = tmp->next;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev != NULL)
+			{
+				tmp->prev->next = tmp;
+			}
+			else
+			{
+				*list = tmp;
+			}
+			print_list(*list);
+		}
+		tmp = tmp->next;
+	}
+}
