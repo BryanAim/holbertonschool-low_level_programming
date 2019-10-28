91fe915bfb77ded80f4851765a6a99aef5a8943d
jucortesa <984@holbertonschool.com>
2019-10-28 23:21:33 +0000

    [Add] counting sort

---
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ * counting_sort - counting sort
+ * @array: pointer to array
+ * @size: size of the array
+ **/
+void counting_sort(int *array, size_t size)
+{
+	unsigned int *count, *output;
+	size_t i, max;
+
+	max = array[0];
+
+	for (i = 0; i < size; i++)
+	{
+		if ((unsigned int)array[i] > max)
+			max = array[i];
+	}
+	count = malloc(max * sizeof(size_t));
+	output = malloc(size * sizeof(size_t));
+	if (count == NULL)
+		return;
+	if (output == NULL)
+		return;
+	for (i = 0; i <= max; ++i)
+		count[i] = 0;
+	for (i = 0; i < size; i++)
+	{
+		count[array[i]] += 1;
+	}
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+	print_array((const int *)count, max + 1);
+	for (i = 0; i < size; i++)
+	{
+		output[count[array[i]] - 1] = array[i];
+		--count[array[i]];
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(count);
+	free(output);
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -42,5 +42,5 @@ void swap_right_to_left(listint_t **list, listint_t *tmp);
 
 void merge(int *array, int *left, int *right, size_t size);
 void merge_sort(int *array, size_t size);
-
+void counting_sort(int *array, size_t size);
 #endif
