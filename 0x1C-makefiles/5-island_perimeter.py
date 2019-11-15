e3d3ba2b7fcc76deca8c4360e76033d14185c034
JUCORTESA <984@holbertonschool.com>
2019-11-15 22:12:08 +0000

    [Add] island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""File representing an island"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island
+        perimeter is 0 if not 1 finded in the array, means there is no island,
+        each time it has a land near it reduce the perimeter value by one"""
+    perimeter = 0
+    for i in range(len(grid)):
+        only = 0
+        for j in range(len(grid[i])):
+            flag = 0
+            perimeter += 4
+            if (grid[i][j] == 1):
+                only = 1
+                try:
+                    if (grid[i + 1][j] == 1):
+                        perimeter -= 1
+                        flag += 1
+                        only += 1
+                except:
+                    return 0
+                try:
+                    if (grid[i - 1][j] == 1):
+                        perimeter -= 1
+                        flag += 1
+                        only += 1
+                except:
+                    return 0
+                try:
+                    if (grid[i][j + 1] == 1):
+                        perimeter -= 1
+                        flag += 1
+                        only += 1
+                except:
+                    return 0
+                try:
+                    if (grid[i][j - 1] == 1):
+                        perimeter -= 1
+                        flag += 1
+                        only += 1
+                except:
+                    return 0
+            if (flag == 0):
+                perimeter -= 4
+        if (only == 1):
+            """just one square surronded by water """
+            return 4
+    return perimeter
