4e07700f9f1914e120ad7aa01403f78efcb56127
JUCORTESA <984@holbertonschool.com>
2019-12-06 20:37:36 +0000

    [fix]

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -8,16 +8,12 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
-
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	left_height = binary_tree_is_perfect(tree->left)
-		&& binary_tree_height(tree->left);
-	right_height = binary_tree_is_perfect(tree->right)
-		&& binary_tree_height(tree->right);
-	return (left_height == right_height);
+
+	return (binary_tree_is_perfect(tree->left) &&
+		 binary_tree_is_perfect(tree->right) &&
+		(binary_tree_height(tree->left) == binary_tree_height(tree->right)));
 }
